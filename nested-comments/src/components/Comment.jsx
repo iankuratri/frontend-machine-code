7fc05176a81f054b43c858c351fc63e3c97c3b40
Ankur Atri <ankuratri.in@gmail.com>
2023-10-09 21:43:33 +0530

    Nested comments ui done

---
+++ nested-comments/src/components/Comment.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./comment.css";
+
+const Comment = ({ comment, onAdd }) => {
+  const [showInput, setShowInput] = useState(false);
+  const [input, setInput] = useState("");
+
+  const addComment = () => {
+    const newComment = {
+      id: Date.now(),
+      text: input,
+      replies: [],
+    };
+
+    onAdd(comment.id, newComment);
+
+    setInput("");
+    setShowInput(false);
+  };
+
+  return (
+    <>
+      <div className="comment-container">
+        <p>{comment.text}</p>
+
+        {!showInput ? (
+          <div>
+            <button onClick={() => setShowInput(true)}>Add Reply</button>
+          </div>
+        ) : (
+          <>
+            <input
+              type="text"
+              name="comment"
+              value={input}
+              placeholder="Add your comment here..."
+              onChange={(event) => setInput(event.target.value)}
+            />
+
+            <div className="btn-container">
+              <button onClick={addComment}>Reply</button>
+              <button onClick={() => setShowInput(false)}>Cancel</button>
+            </div>
+          </>
+        )}
+      </div>
+
+      {comment.replies.map((comment) => (
+        <div className="comment-reply" key={comment.id}>
+          <Comment comment={comment} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Comment;
---
+++ nested-comments/src/data/commentData.js
@@ -0,0 +1,60 @@
+export const commentData = [
+  {
+    id: "1",
+    text: "Hello world! How are you?",
+    replies: [
+      {
+        id: "2",
+        text: "Hey, I am fine, wau?",
+        replies: [
+          {
+            id: "3",
+            text: "Hello, I am food, How are you?",
+            replies: [
+              {
+                id: "4",
+                text: "Life sucks man!",
+                replies: [
+                  {
+                    id: "5",
+                    text: "Haha, I can relate :)",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: "11",
+    text: "Hello world! How are you?",
+    replies: [
+      {
+        id: "21",
+        text: "Hey, I am fine, wau?",
+        replies: [
+          {
+            id: "31",
+            text: "Hello, I am food, How are you?",
+            replies: [
+              {
+                id: "41",
+                text: "Life sucks man!",
+                replies: [
+                  {
+                    id: "51",
+                    text: "Haha, I can relate :)",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
